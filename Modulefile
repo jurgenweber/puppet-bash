31ed97e4612e9e531dc9448e5262a49770ab2bf2
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2011-10-13 06:56:55 +0200

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,9 @@
+name 'dhoppe-bash'
+version '0.0.1'
+
+author 'Dennis Hoppe'
+license 'GPLv2'
+project_page 'http://github.com/dhoppe/puppet-bash'
+source 'git://github.com/dhoppe/puppet-bash'
+summary 'BASH Module'
+description 'BASH Module for Debian, Ubuntu'
---
+++ manifests/init.pp
@@ -0,0 +1,25 @@
+class bash {
+	define bash::prompt($color = false) {
+		$t_color = $color ? {
+			false   => '\033[01;32m',
+			default => $color,
+		}
+
+		file { "$name":
+			owner   => root,
+			group   => root,
+			mode    => 0644,
+			content => template("bash/$lsbdistcodename/etc/skel/bashrc.erb"),
+		}
+	}
+
+	bash::prompt { "/etc/skel/.bashrc":
+		color => '\033[01;32m',
+	}
+
+	bash::prompt { "/root/.bashrc":
+		color => '\033[01;31m',
+	}
+}
+
+# vim: tabstop=3
