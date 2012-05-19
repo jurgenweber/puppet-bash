7bc46f93c8c0e85653969883b11d732e7e8dad9a
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-05-19 09:08:00 +0200

    Fix several issues mentioned at Puppet style guide

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,16 @@
-class bash {
+class bash (
+  $color_green = $bash::params::green,
+  $color_red   = $bash::params::red
+) inherits bash::params {
+
   validate_string(hiera('green'))
   validate_string(hiera('red'))
 
   bash::prompt { '/etc/skel/.bashrc':
-    color => hiera('green'),
+    color => $color_green,
   }
 
   bash::prompt { '/root/.bashrc':
-    color => hiera('red'),
+    color => $color_red,
   }
 }
---
+++ manifests/params.pp
@@ -0,0 +1,4 @@
+class bash::params {
+  $green = hiera('green')
+  $red   = hiera('red')
+}
