6e772460dbb330d9b654fae8710e09d505e42d75
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-05-19 09:08:00 +0200

    Fix several issues mentioned at Puppet style guide

--- manifests/prompt.pp
+++ manifests/prompt.pp
@@ -1,12 +1,7 @@
-define bash::prompt($color = false) {
-  $t_color = $color ? {
-    false   => '\033[01;32m',
-    default => $color,
-  }
-
+define bash::prompt($color) {
   file { $name:
-    owner   => root,
-    group   => root,
+    owner   => 'root',
+    group   => 'root',
     mode    => '0644',
     content => template("bash/${::lsbdistcodename}/etc/skel/bashrc.erb"),
   }
