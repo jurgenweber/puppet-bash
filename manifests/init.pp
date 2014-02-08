903e2d23da800302305977abd37268e3f87676c1
Jurgen Weber <j.weber@foodpanda.com>
2014-02-08 11:59:04 +0100

    adding in some options for bashrc

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 class bash (
   $color_green = $bash::params::green,
-  $color_red   = $bash::params::red
+  $color_red   = $bash::params::red,
+  $options     = params_lookup( 'options' ),
 ) inherits bash::params {
 
   bash::prompt { '/etc/skel/.bashrc':
