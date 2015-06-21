26ca9dca8369a536d4d1fa6c335f9bdd25ce5d20
Jurgen Weber <j.weber@foodpanda.com>
2015-06-21 08:46:30 +0200

    allow for prompts

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,10 @@
 #
 #
 class bash (
-  $color_green = $bash::params::green,
-  $color_red   = $bash::params::red,
+  $color_green    = $bash::params::green,
+  $color_red      = $bash::params::red,
+  $root_prompt    = undef,
+  $default_prompt = undef,
 ) inherits params {
 
   package { $params::packages:
@@ -11,11 +13,13 @@ class bash (
   }
 
   bash::prompt { '/etc/skel/.bashrc':
-    color => $color_green,
+    color  => $bash::params::green,
+    prompt => $default_prompt,
   }
 
   bash::prompt { '/root/.bashrc':
-    color => $color_red,
+    color  => $bash::params::red,
+    prompt => $root_prompt,
   }
 
   class { 'bash::aliases': }
